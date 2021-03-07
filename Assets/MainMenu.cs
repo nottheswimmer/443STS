62a060163ba2eb19f8f408f3adb9ede2e36a6f4a
Michael Phelps <michael.phelps@cpcc.edu>
2021-03-06 19:51:57 -0500

    Add start menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        var nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("The player pressed the quit button.");
+        Application.Quit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# 443STS
+
+A video game, hopefully.
+
+## References
+
+- [START MENU in Unity](https://www.youtube.com/watch?v=zc8ac_qUXQY)
